25ffe8f06c407fd73424b5d217dc1092d9cbdace
kwonws <kevincool6@naver.com>
2025-01-03 16:33:03 +0900

    테이블뷰컨트롤러 커스텀셀 구성

---
+++ SecondWeak/UserTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  UserTableViewCell.swift
+//  SecondWeak
+//
+//  Created by 권우석 on 1/3/25.
+//
+
+import UIKit
+
+class UserTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var messageLabel: UILabel!
+}
